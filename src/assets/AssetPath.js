f01d38268bc2fadf63a4f1663f5852dc989c08a2
Utkarsh-K10 <utkarshkushwah9@gmail.com>
2025-05-03 13:06:55 +0530

    Add AssetPath.js to manage asset URLs for gallery, logos, and admissions

---
+++ src/assets/AssetPath.js
@@ -0,0 +1,32 @@
+
+const galleryPaths = {
+    gallery6: "https://res.cloudinary.com/dc2qoqcrd/image/upload/v1746200168/gallery%20section/ixvwkst4jz4tagnreiyz.jpg",
+    gallery5: "https://res.cloudinary.com/dc2qoqcrd/image/upload/v1746200168/gallery%20section/epgqdushi043re6n2rxd.jpg",
+    gallery4: "https://res.cloudinary.com/dc2qoqcrd/image/upload/v1746200167/gallery%20section/sf4ajhimnzc1ipp0gmbp.png",
+    gallery3: "https://res.cloudinary.com/dc2qoqcrd/image/upload/v1746200639/gallery%20section/mjvll73fq28h3nefyqev.png",
+    gallery2: "https://res.cloudinary.com/dc2qoqcrd/image/upload/v1745733388/gallery%20section/gejsidxei7pqbxtcmdlp.png",
+    gallery1: "https://res.cloudinary.com/dc2qoqcrd/image/upload/v1746200165/gallery%20section/ichf1le3zncgsak7gysb.jpg",
+    homeSection1: "https://res.cloudinary.com/dc2qoqcrd/image/upload/v1746200639/gallery%20section/mjvll73fq28h3nefyqev.png",
+};
+
+const logoPaths = {
+    logo: "https://res.cloudinary.com/dc2qoqcrd/image/upload/v1746244178/gallery%20section/trwo2kgnrzolixvma8mv.png",
+    svgLogo: "https://res.cloudinary.com/dc2qoqcrd/image/upload/v1746244440/gallery%20section/c7hifsgkosldnfmobdu4.svg",
+    qrCode: "https://res.cloudinary.com/dc2qoqcrd/image/upload/v1746244636/gallery%20section/jltyxr7h5j6yjrtmjb3h.png",
+};
+
+const AboutUsPaths = {
+    collage1: "https://res.cloudinary.com/dc2qoqcrd/image/upload/v1746251191/gallery%20section/dimydbt5bcbw5s468afh.jpg",
+    collage2: "https://res.cloudinary.com/dc2qoqcrd/image/upload/v1746244635/gallery%20section/gwsbpdwtz0plz1nyqdwx.webp",
+    classroom: "https://res.cloudinary.com/dc2qoqcrd/image/upload/v1746244635/gallery%20section/ifc4uvncfsjscidpizv3.webp",
+    studentLife: "https://res.cloudinary.com/dc2qoqcrd/image/upload/v1746244636/gallery%20section/afdgqgzaks2hgquz1afj.webp",
+    studentinLab: "https://res.cloudinary.com/dc2qoqcrd/image/upload/c_crop,g_auto,h_800,w_800/gallery%20section/v2sai9lfjt1zldqgq4bq",
+    playground: "https://res.cloudinary.com/dc2qoqcrd/image/upload/v1746244636/gallery%20section/bilcxozzh8ahbdtzzok2.webp",
+}
+
+const AdmissionPaths = {
+    admissionBanner: "https://res.cloudinary.com/dc2qoqcrd/image/upload/v1746251606/gallery%20section/zlhwfcj6r965aqvlyih9.jpg",
+    classroomImage: "https://res.cloudinary.com/dc2qoqcrd/image/upload/e_improve:outdoor/gallery%20section/z9g8bzzrsyko6axsg3su",
+};
+
+export { galleryPaths, logoPaths, AboutUsPaths, AdmissionPaths };
